c5d8af86be60a5bbc26e0269a5c73a3d9ab295c5
Kefiloe Moshoaliba <90796051+Moshoaliba@users.noreply.github.com>
2022-10-17 23:55:32 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace CellNumber_creator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDone = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.lblDisplay = new System.Windows.Forms.Label();
+            this.lblDsiplay1 = new System.Windows.Forms.Label();
+            this.lblDisplay2 = new System.Windows.Forms.Label();
+            this.lblDsiplay3 = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtInput2 = new System.Windows.Forms.TextBox();
+            this.txtInput1 = new System.Windows.Forms.TextBox();
+            this.txtInput3 = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.saveD = new System.Windows.Forms.SaveFileDialog();
+            this.openD = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnDone
+            // 
+            this.btnDone.Location = new System.Drawing.Point(12, 166);
+            this.btnDone.Name = "btnDone";
+            this.btnDone.Size = new System.Drawing.Size(75, 23);
+            this.btnDone.TabIndex = 0;
+            this.btnDone.Text = "Done";
+            this.btnDone.UseVisualStyleBackColor = true;
+            this.btnDone.Click += new System.EventHandler(this.btnDone_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(124, 166);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(70, 17);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Vodacom";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 58);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(49, 17);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "MTN";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(6, 95);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(52, 17);
+            this.radioButton3.TabIndex = 4;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Cell C";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Location = new System.Drawing.Point(329, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 129);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Network preferred:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(12, 198);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(443, 229);
+            this.listBox1.TabIndex = 6;
+            // 
+            // lblDisplay
+            // 
+            this.lblDisplay.AutoSize = true;
+            this.lblDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDisplay.Location = new System.Drawing.Point(0, 15);
+            this.lblDisplay.Name = "lblDisplay";
+            this.lblDisplay.Size = new System.Drawing.Size(44, 15);
+            this.lblDisplay.TabIndex = 7;
+            this.lblDisplay.Text = "Name:";
+            // 
+            // lblDsiplay1
+            // 
+            this.lblDsiplay1.AutoSize = true;
+            this.lblDsiplay1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDsiplay1.Location = new System.Drawing.Point(0, 38);
+            this.lblDsiplay1.Name = "lblDsiplay1";
+            this.lblDsiplay1.Size = new System.Drawing.Size(61, 15);
+            this.lblDsiplay1.TabIndex = 8;
+            this.lblDsiplay1.Text = "Surname:";
+            // 
+            // lblDisplay2
+            // 
+            this.lblDisplay2.AutoSize = true;
+            this.lblDisplay2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDisplay2.Location = new System.Drawing.Point(0, 62);
+            this.lblDisplay2.Name = "lblDisplay2";
+            this.lblDisplay2.Size = new System.Drawing.Size(31, 15);
+            this.lblDisplay2.TabIndex = 9;
+            this.lblDisplay2.Text = "Age:";
+            // 
+            // lblDsiplay3
+            // 
+            this.lblDsiplay3.AutoSize = true;
+            this.lblDsiplay3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDsiplay3.Location = new System.Drawing.Point(0, 90);
+            this.lblDsiplay3.Name = "lblDsiplay3";
+            this.lblDsiplay3.Size = new System.Drawing.Size(72, 15);
+            this.lblDsiplay3.TabIndex = 10;
+            this.lblDsiplay3.Text = "Old Cell.No:";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(65, 14);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(213, 20);
+            this.txtInput.TabIndex = 11;
+            // 
+            // txtInput2
+            // 
+            this.txtInput2.Location = new System.Drawing.Point(37, 66);
+            this.txtInput2.Name = "txtInput2";
+            this.txtInput2.Size = new System.Drawing.Size(103, 20);
+            this.txtInput2.TabIndex = 12;
+            // 
+            // txtInput1
+            // 
+            this.txtInput1.Location = new System.Drawing.Point(67, 40);
+            this.txtInput1.Name = "txtInput1";
+            this.txtInput1.Size = new System.Drawing.Size(211, 20);
+            this.txtInput1.TabIndex = 13;
+            // 
+            // txtInput3
+            // 
+            this.txtInput3.Location = new System.Drawing.Point(78, 90);
+            this.txtInput3.Name = "txtInput3";
+            this.txtInput3.Size = new System.Drawing.Size(154, 20);
+            this.txtInput3.TabIndex = 14;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(229, 166);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 15;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // openD
+            // 
+            this.openD.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.txtInput3);
+            this.Controls.Add(this.txtInput1);
+            this.Controls.Add(this.txtInput2);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblDsiplay3);
+            this.Controls.Add(this.lblDisplay2);
+            this.Controls.Add(this.lblDsiplay1);
+            this.Controls.Add(this.lblDisplay);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnDone);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDone;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label lblDisplay;
+        private System.Windows.Forms.Label lblDsiplay1;
+        private System.Windows.Forms.Label lblDisplay2;
+        private System.Windows.Forms.Label lblDsiplay3;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtInput2;
+        private System.Windows.Forms.TextBox txtInput1;
+        private System.Windows.Forms.TextBox txtInput3;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.SaveFileDialog saveD;
+        private System.Windows.Forms.OpenFileDialog openD;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,131 @@
+﻿//Moshoaliba,K-34316345
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+using System.Data.OleDb;
+
+namespace CellNumber_creator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDone_Click(object sender, EventArgs e)
+        {
+            if(int.TryParse(txtInput2.Text,out int age) && int.TryParse(txtInput3.Text,out int num))
+            {
+                SaveFile();
+            }
+            else
+            {
+                MessageBox.Show("incorrect input!!");
+            }
+        }
+
+        private void SaveFile()
+        {
+            try
+            {
+                int randNumber;
+                Random rand = new Random();
+                randNumber = rand.Next(1000000, 3000001);
+
+                const double price = 3.50;
+
+                StreamWriter outputFile;
+                
+                if(saveD.ShowDialog() == DialogResult.OK)
+                {
+                    string path = saveD.FileName;
+                    outputFile = File.CreateText(path);
+                    outputFile.WriteLine("A simcard has been requested by " + txtInput.Text+" "+txtInput1.Text);
+                    outputFile.WriteLine("Who is " + txtInput2.Text + " years ");
+                    outputFile.WriteLine("\nThe details are as follows:");
+                    outputFile.WriteLine("old numbers: " + txtInput3.Text);
+                    if(radioButton1.Checked)
+                    {
+                        outputFile.WriteLine("New numbers: 076" + randNumber);
+                        outputFile.WriteLine("Network: Vodacom");
+                        outputFile.WriteLine("Price: R" +price*5);
+                    }
+                    else if(radioButton2.Checked)
+                    {
+                        outputFile.WriteLine("New numbers: 064" + randNumber);
+                        outputFile.WriteLine("Network: MTN");
+                        outputFile.WriteLine("Price: R" + price * 3);
+                    }
+                    else if(radioButton3.Checked)
+                    {
+                        outputFile.WriteLine("New numbers: 082" + randNumber);
+                        outputFile.WriteLine("Network: Cell C");
+                        outputFile.WriteLine("Price: R" + price * 1.50);
+                    }
+
+                    outputFile.Close();
+                    MessageBox.Show("Done!!");
+
+                }
+                else
+                {
+                    MessageBox.Show("No file selected");
+                }
+
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("Error while trying to save !!");
+            }
+        }
+
+        private void OpenFile()
+        {
+            StreamReader readFile;
+
+            try
+            {
+                if(openD.ShowDialog() == DialogResult.OK)
+                {
+                    string path = openD.FileName;
+                    readFile = File.OpenText(path);
+
+                    while(!readFile.EndOfStream)
+                    {
+                        listBox1.Items.Add(readFile.ReadLine());
+                    }
+
+                    readFile.Close();
+                }
+                else
+                {
+                    MessageBox.Show("No path selected!!");
+                }
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("Error while opening file!!");
+            }
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFile();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
